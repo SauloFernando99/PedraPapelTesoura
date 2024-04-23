6e5e45852b1022aff50f20dced83835e73bc2025
SauloFernando99 <saulofernando114@gmail.com>
2024-04-23 20:24:25 -0300

    feat: new view for Game screen

--- app/src/main/java/com/example/pedrapapeltesoura/GameActivity.kt
+++ app/src/main/java/com/example/pedrapapeltesoura/GameActivity.kt
@@ -1,4 +1,17 @@
 package com.example.pedrapapeltesoura
 
-class GameActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pedrapapeltesoura.databinding.ActivityGameBinding
+
+class GameActivity : AppCompatActivity() {
+
+    private val agb: ActivityGameBinding by lazy {
+        ActivityGameBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(agb.root)
+    }
 }
