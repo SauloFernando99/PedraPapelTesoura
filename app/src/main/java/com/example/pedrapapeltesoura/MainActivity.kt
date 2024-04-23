3f8d65d532cc923c1f3ddaa16fefbae8feaa5da4
SauloFernando99 <saulofernando114@gmail.com>
2024-04-23 20:49:58 -0300

    feat: implemented the buttons system to access the GameActivity sending the number of players

--- app/src/main/java/com/example/pedrapapeltesoura/MainActivity.kt
+++ app/src/main/java/com/example/pedrapapeltesoura/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.pedrapapeltesoura
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pedrapapeltesoura.Constant.NUM_PLAYERS
 import com.example.pedrapapeltesoura.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+        amb.toolbarIn.toolbar.apply {
+            subtitle = this@MainActivity.javaClass.simpleName
+            setSupportActionBar(this)
+        }
+
+        amb.twoPlayersBtn.setOnClickListener {
+            openPlayerSelectionActivity(2)
+        }
+
+        amb.threePlayersBtn.setOnClickListener {
+            openPlayerSelectionActivity(3)
+        }
+    }
+
+    private fun openPlayerSelectionActivity(numPlayers: Int) {
+        val viewGameIntent = Intent(this@MainActivity, GameActivity::class.java)
+        viewGameIntent.putExtra(NUM_PLAYERS, numPlayers)
+        startActivity(viewGameIntent)
     }
 }
